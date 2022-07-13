e95019b8637f3246071109740c2d8542dcd5f7f1
QIDI TECH <37525539+QIDITECH@users.noreply.github.com>
2022-07-13 09:48:38 +0800

    Add files via upload

---
+++ plugins/CustomSupports/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2018 Lokster <http://lokspace.eu>
+# Based on the SupportBlocker plugin by Ultimaker B.V., and licensed under LGPLv3 or higher.
+
+from . import CustomSupports
+
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("cura")
+
+def getMetaData():
+    return {
+        "tool": {
+            "name": i18n_catalog.i18nc("@label", "Custom Supports"),
+            "description": i18n_catalog.i18nc("@info:tooltip", "Add custom supports"),
+            "icon": "tool_icon.svg",
+            "tool_panel": "CustomSupports.qml",
+            "weight": 4
+        }
+    }
+
+def register(app):
+    return { "tool": CustomSupports.CustomSupports() }
---
+++ plugins/FileHandlers/STLWriter/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2015 Ultimaker B.V.
+# Uranium is released under the terms of the LGPLv3 or higher.
+
+from . import STLWriter
+
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("uranium")
+
+def getMetaData():
+    return {
+        "mesh_writer": {
+            "output": [
+                {
+                    "mime_type": "application/x-stl-ascii",
+                    "mode": STLWriter.STLWriter.OutputMode.TextMode,
+                    "extension": "stl",
+                    "description": i18n_catalog.i18nc("@item:inlistbox", "STL File (ASCII)")
+                },
+                {
+                    "mime_type": "application/x-stl-binary",
+                    "mode": STLWriter.STLWriter.OutputMode.BinaryMode,
+                    "extension": "stl",
+                    "description": i18n_catalog.i18nc("@item:inlistbox", "STL File (Binary)")
+                }
+            ]
+        }
+    }
+
+def register(app):
+    return { "mesh_writer": STLWriter.STLWriter() }
