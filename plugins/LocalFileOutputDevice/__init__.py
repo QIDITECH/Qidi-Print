c38d31042307dbe0c8a1cb76218a9a6e04bf8cad
QIDI TECH <37525539+QIDITECH@users.noreply.github.com>
2022-07-13 09:52:14 +0800

    Add files via upload

---
+++ plugins/LocalFileOutputDevice/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2015 Ultimaker B.V.
+# Uranium is released under the terms of the LGPLv3 or higher.
+
+from . import LocalFileOutputDevicePlugin
+
+from UM.i18n import i18nCatalog
+catalog = i18nCatalog("uranium")
+
+def getMetaData():
+    return {
+    }
+
+def register(app):
+    return { "output_device": LocalFileOutputDevicePlugin.LocalFileOutputDevicePlugin() }
