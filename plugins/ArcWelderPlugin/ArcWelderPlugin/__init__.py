01ebfaa57088322c9f517fc6b14548aaf076036c
whitekaren <130121731+whitekaren@users.noreply.github.com>
2023-04-07 13:27:00 +0800

    Add files via upload

---
+++ plugins/ArcWelderPlugin/ArcWelderPlugin/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 Aldo Hoeben / fieldOfView
+# The ArcWelderPlugin for Cura is released under the terms of the AGPLv3 or higher.
+
+from . import ArcWelderPlugin
+
+
+def getMetaData():
+    return {}
+
+
+def register(app):
+    return {"extension": ArcWelderPlugin.ArcWelderPlugin()}
