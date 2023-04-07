24bf8111868e6be4539303004bf6b124a8a3866d
whitekaren <130121731+whitekaren@users.noreply.github.com>
2023-04-07 13:22:50 +0800

    Delete python directory

--- python/cura/CuraVersion.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
-
-CuraVersion = "5.6.14"
-CuraBuildType = ""
-CuraDebugMode = True if "" == "ON" else False
